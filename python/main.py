d4b1b8f23a497880238c26cf76a94d960b24dd07
daisyarabella <purplewoof1@gmail.com>
2016-12-07 17:53:14 +0000

    getting both p and q values

--- python/main.py
+++ python/main.py
@@ -5,36 +5,44 @@ import math
 
 def main():
 	
+	# Get and read in data 
 	data = pd.read_csv('../data/linearEqs.csv')
 	print(data)
 	print('\n')
 
-	# Get all of the 'solutions' to linear equations in matrix B
-	matrixB = data['S(t+1)']
+	# Get all of the 'solutions' to linear equations in solution matrix
+	solutions = data['S(t+1)']
 
-	# Put the coefficient data of all linear equations in matrix A
-	matrixA = data['aCo']
-	matrixA = matrixA.to_frame()
-	matrixA['bCo'] = data['bCo']
-	matrixA['cCo'] = data['cCo']
+	# Put the coefficient data of all linear equations in coefficient matrix
+	coeff = data['aCo']
+	coeff = coeff.to_frame()
+	coeff['bCo'] = data['bCo']
+	coeff['cCo'] = data['cCo']
 
 	# Solve system of linear equations and print values for a, b and c
-	a, b, c = np.linalg.lstsq(matrixA.as_matrix(), matrixB.as_matrix())[0]
+	a, b, c = np.linalg.lstsq(coeff.as_matrix(), solutions.as_matrix())[0]
 	print('a = ' + str(a))
 	print('b = ' + str(b))
-	print('c = ' + str(c))
+	print('c = ' + str(c)+'\n')
 
-	#m = (-b-math.sqrt((b*b)-4*a*c))/(2*a)
+	# Find p and q based on mMinus
+	mMinus = (-b-math.sqrt((b*b)-4*a*c))/(2*a)
+	print('m = (-b-sqrt(b^2-4ac))/2a = ' + str(mMinus))
+	pMinus = a/mMinus
+	qMinus = b + pMinus
+	print('p- = a/m = '+str(pMinus))
+	print('q- = b+p = '+str(qMinus)+'\n')
+
+	# Find p and q based on mPlus
+	mPlus = (-b+math.sqrt((b*b)-4*a*c))/(2*a)
+	print('m = (-b+sqrt(b^2-4ac))/2a = ' + str(mPlus))
+	pPlus = a/mPlus	
+	qPlus = b + pPlus
+	print('p+ = a/m = '+str(pPlus))
+	print('q+ = b+p = '+str(qPlus)+'\n')
+
 	# Set m as the total number of adopters
-	m = data.tail(1)['bCo'].values
-	print('m = ' + str(m))
-	
-	# Calculate and print p and q
-	p = a/m	
-	q = b + p
-	print('\np = a/m = '+str(p))
-	print('q = b+p = '+str(q))
-
+	#m = data.tail(1)['bCo'].values
 
 if __name__ == "__main__":
 	main()
